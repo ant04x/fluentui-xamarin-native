1c482a855c07bb2f23eaac115a9d6ee6ff082716
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-09-29 00:48:18 +0200

    Added themes and Theming namespace completed.

---
+++ Theming/FluentUIContextThemeWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.View;
+using Android.Views;
+using Android.Widget;
+
+
+
+namespace FluentUI.Theming
+{
+    public class FluentUIContextThemeWrapper : Android.Support.V7.View.ContextThemeWrapper
+    {
+        public FluentUIContextThemeWrapper(Context base_) : base(base_, Resource.Style.Theme_FluentUI) { }
+
+        protected override void OnApplyThemeResource(Resources.Theme theme, int resid, bool first)
+        {
+            theme.ApplyStyle(resid, false);
+        }
+    }
+}
