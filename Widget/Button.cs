3c6333c44ef38f15f28abe7740d9a922c6fd07c5
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-09-29 00:49:04 +0200

    Added Widget Button.

---
+++ Widget/Button.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.Support.V7.Widget;
+using Android.Util;
+using FluentUI.Theming;
+
+namespace FluentUI.Widget
+{
+    public class Button : AppCompatButton
+    {
+        public Button(Context context, IAttributeSet? attrs = null, int? defStyleAttr = null) : base(new FluentUIContextThemeWrapper(context), attrs, (int)defStyleAttr)
+        {
+            _ = defStyleAttr ?? Resource.Attribute.buttonStyle;
+        }
+    }
+}
