e12151e823f8f5ea1aef34f5bb80a9ee40d786f0
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-09-29 02:10:57 +0200

    Added Widget ProgressBar and BottomNavigation with the Dimens.

---
+++ Widget/BottomNavigation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.Support.V7.Widget;
+using Android.Util;
+using FluentUI.Theming;
+using Android.Support.Design.BottomNavigation;
+
+namespace FluentUI.Widget
+{
+    public class BottomNavigationView : Android.Support.Design.Widget.BottomNavigationView
+    {
+        private int userIconSize = 0;
+        private int defaultIconSize = 0;
+
+        public BottomNavigationView(Context context, IAttributeSet? attrs = null, int defStyleAttr = 0) 
+            : base(new FluentUIContextThemeWrapper(context), attrs, defStyleAttr) { }
+
+        public override int LabelVisibilityMode {
+            set 
+            {
+                if (userIconSize == 0)
+                {
+                    defaultIconSize = AdjustIconSize();
+                    userIconSize = ItemIconSize;
+                }
+
+                base.LabelVisibilityMode = value;
+                ItemIconSize = AdjustIconSize();
+            }
+        }
+
+        private int AdjustIconSize()
+        {
+            if (userIconSize != defaultIconSize)
+                return userIconSize;
+            else if (LabelVisibilityMode == Android.Support.Design.BottomNavigation.LabelVisibilityMode.LabelVisibilityUnlabeled)
+                return Resources.GetDimensionPixelSize(Resource.Dimension.fluentui_bottom_navigation_icon_unlabeled);
+            else
+                return Resources.GetDimensionPixelSize(Resource.Dimension.fluentui_bottom_navigation_icon_labeled);
+        }
+    }
+}
---
+++ Widget/ProgressBar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.Support.V7.Widget;
+using Android.Util;
+using FluentUI.Theming;
+
+namespace FluentUI.Widget
+{
+    public class ProgressBar : Android.Widget.ProgressBar
+    {
+        public ProgressBar(Context context, IAttributeSet? attrs = null, int defStyleAttr = 0, int defStyleRes = 0)
+            : base(new FluentUIContextThemeWrapper(context), attrs, defStyleAttr, defStyleRes) { }
+    }
+}
