25789ee00a12534fc7ead0465816a8690f8f8302
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-09-29 14:03:40 +0200

    Not completed WrapContentViewPager update.

--- View/WrapcontentViewPager.cs
+++ View/WrapcontentViewPager.cs
@@ -18,12 +18,27 @@ using Java.Lang;
 
 namespace FluentUI.View
 {
-    public class WrapcontentViewPager : ViewPager
+    public class WrapContentViewPager : ViewPager
     {
-        private static Property HeightProperty
+        public WrapContentViewPager(Context context, IAttributeSet? attrs = null)
+            : base(context, attrs) { }
+
+        private class ObjectProperty : Property
         {
-            set;
-            get;
-        } = new Java.Lang.Object();
+            public ObjectProperty(Class? type, string? name) { }
+            protected ObjectProperty(IntPtr javaReference, JniHandleOwnership transfer) { }
+
+            public override Java.Lang.Object? Get(Java.Lang.Object? @object)
+            {
+
+            }
+
+            public override void Set(Java.Lang.Object? @object, Java.Lang.Object? value)
+            {
+
+            }
+        }
+
+        private static readonly Property heightProperty = new ObjectProperty(Class.FromType(typeof(int)), "height");
     }
 }
