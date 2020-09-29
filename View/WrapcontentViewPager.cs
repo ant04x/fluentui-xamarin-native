62d088d7ac60e002bebcf93db59980a70419c445
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-09-29 11:52:54 +0200

    Not completed WrapContentViewManager

---
+++ View/WrapcontentViewPager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.Support.V7.Widget;
+using Android.Util;
+using FluentUI.Theming;
+using Android.Support.V4.View;
+using Java.Lang;
+
+namespace FluentUI.View
+{
+    public class WrapcontentViewPager : ViewPager
+    {
+        private static Property HeightProperty
+        {
+            set;
+            get;
+        } = new Java.Lang.Object();
+    }
+}
