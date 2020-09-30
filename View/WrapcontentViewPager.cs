01ef49035878be96777f048900e59d54775e3692
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-09-30 14:51:19 +0200

    Class not completed. All methods added except OnMeasure.

--- View/WrapcontentViewPager.cs
+++ View/WrapcontentViewPager.cs
@@ -15,22 +15,23 @@ using Android.Util;
 using FluentUI.Theming;
 using Android.Support.V4.View;
 using Java.Lang;
+using Android.Animation;
 
 namespace FluentUI.View
 {
     public class WrapContentViewPager : ViewPager
     {
-        public WrapContentViewPager(Context context, IAttributeSet? attrs = null)
+        public WrapContentViewPager(Context context, IAttributeSet attrs = null)
             : base(context, attrs) { }
 
         private class SimpleProperty : Property
         { 
-            public SimpleProperty(Class? type, string? name) : base(type, name) { }
+            public SimpleProperty(Class type, string name) : base(type, name) { }
 
             protected SimpleProperty(IntPtr javaReference, JniHandleOwnership transfer) 
                 : base(javaReference, transfer) { }
 
-            public override void Set(Java.Lang.Object? @object, Java.Lang.Object? value)
+            public override void Set(Java.Lang.Object @object, Java.Lang.Object value)
             {
                 if (value != null)
                 {
@@ -41,12 +42,78 @@ namespace FluentUI.View
             }
 
             // WARNING It does not return a integer.
-            public override Java.Lang.Object? Get(Java.Lang.Object? @object)
+            public override Java.Lang.Object Get(Java.Lang.Object @object)
             {
                 return ((Android.Views.View)@object).MeasuredHeight;
             }
         }
 
         private static readonly Property heightProperty = new SimpleProperty(Class.FromType(typeof(int)), "height");
+
+
+        public Java.Lang.Object currentObject = null;
+
+        private bool shouldWrapContent = true;
+        public bool ShouldWrapContent
+        {
+            get => shouldWrapContent;
+            set
+            {
+                if (shouldWrapContent != value)
+                {
+                    shouldWrapContent = value;
+
+                    if (shouldWrapContent && LayoutParameters != null)
+                    {
+                        ViewGroup.LayoutParams lp = LayoutParameters;
+                        lp.Height = ViewGroup.LayoutParams.WrapContent;
+                        LayoutParameters = lp;
+                    }
+                }
+            }
+        }
+
+        private ObjectAnimator animator = null;
+
+        private Android.Views.View CurrentView
+        {
+            get
+            {
+                PagerAdapter adapter = Adapter != null ? Adapter : null;
+                Java.Lang.Object currentObject = this.currentObject != null ? this.currentObject : null;
+
+                for (int i = 0; i < ChildCount; i++)
+                {
+                    Android.Views.View child = GetChildAt(i);
+                    if (adapter.IsViewFromObject(child, currentObject))
+                        return child;
+                }
+                return null;
+            }
+        }
+
+        public void SmoothlyResize(int targetHeight, Animator.IAnimatorListener listener)
+        {
+            animator?.RemoveAllListeners();
+            animator?.Cancel();
+
+            ShouldWrapContent = false;
+            animator = ObjectAnimator.OfInt(this, heightProperty, (int)heightProperty.Get(this), targetHeight);
+
+            if (listener != null)
+                animator?.AddListener(listener);
+
+            animator?.Start();
+        }
+
+        protected override void OnScrollChanged(int l, int t, int oldl, int oldt)
+        {
+            if (l > oldl)
+                animator?.Cancel();
+
+            base.OnScrollChanged(l, t, oldl, oldt);
+        }
+
+
     }
 }
