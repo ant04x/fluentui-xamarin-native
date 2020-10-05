6e787b462c3c4702ad1fa8437bcc19c13d516612
Ant04X <50796747+ant04x@users.noreply.github.com>
2020-10-06 00:07:01 +0200

    Class not completed. Constants added.

---
+++ View/NumberPicker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FluentUI.View
+{
+    internal class NumberPicker : LinearLayout
+    {
+        const int ButtonIncrement = 1;
+        const int ButtonDecrement = 2;
+
+        private const int VirtualViewIDIncrement = 1;
+        private const int VirtualViewIDToggle = 2;
+        private const int VirtualViewIDDecrement = 3;
+        private const int ToggleValue = 2;
+
+        private const int DefaultSelectorWheelItemCount = 3;
+        
+        private const long DefaultLongPressUpdateInterval = 300;
+
+    }
+}
